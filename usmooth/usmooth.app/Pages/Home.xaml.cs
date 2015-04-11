35d78d6e49a37c957c6ca53a60432ad1a96accc2
Gu Lu (home-dev-desktop) <gemfeeling@hotmail.com>
2015-04-11 09:26:07 +0800

    merge Home screen and Command screen, manage connection and command handling in one page

--- usmooth/usmooth.app/Pages/Home.xaml.cs
+++ usmooth/usmooth.app/Pages/Home.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ucore;
 
 namespace usmooth.app.Pages
 {
@@ -21,19 +22,27 @@ namespace usmooth.app.Pages
     public partial class Home : UserControl
     {
         private QotdClient _client;
-        private ushort _clientPort = 5555;
-        //private string _clientHost = "127.0.0.1";
-        private string _clientHost = "10.20.98.91";
-
 
         public Home()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 100; i++)
+            {
+                bb_logging.BBCode += "client is initialized successfully.\r\n";
+            }
+            m_loggingPanel.ScrollToBottom();
         }
 
         private void bt_connect_Click(object sender, RoutedEventArgs e)
         {
-            _client = new QotdClient(_clientHost, _clientPort, AddToLog);
+            if (cb_targetIP.Text.Length == 0)
+                return;
+            string[] info = cb_targetIP.Text.Split(':');
+            if (info.Length != 2)
+                return;
+
+            _client = new QotdClient(info[0], (ushort)EzConv.ToInt(info[1]), AddToLog);
         }
         private void AddToLog(string text)
         {
