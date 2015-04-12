64bdc88b2fe165bdfad68c90a48a0641bb2ac0fe
Gu Lu (home-dev-desktop) <gemfeeling@hotmail.com>
2015-04-12 20:22:15 +0800

    创建 Realtime 页面的布局

--- usmooth/usmooth.app/Pages/Realtime.xaml.cs
+++ usmooth/usmooth.app/Pages/Realtime.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,30 @@ using System.Windows.Shapes;
 
 namespace usmooth.app.Pages
 {
+    public class MeshObject
+    {
+        public string Name { get; set; }
+        public string MeshName { get; set; }
+        public int VertCnt { get; set; }
+        public int MatCnt { get; set; }
+        public float Size { get; set; }
+    }
+
+    public class MaterialObject
+    {
+        public string Name { get; set; }
+        public string ShaderName { get; set; }
+        public int RefCnt { get; set; }
+    }
+
+    public class TextureObject
+    {
+        public string Name { get; set; }
+        public string PixelSize { get; set; }
+        public int RefCnt { get; set; }
+        public string MemSize { get; set; }
+    }
+
     /// <summary>
     /// Interaction logic for Realtime.xaml
     /// </summary>
@@ -23,6 +48,33 @@ namespace usmooth.app.Pages
         public Realtime()
         {
             InitializeComponent();
+
+            MeshGrid.DataContext = GetMeshCollection();
+            MaterialGrid.DataContext = GetMaterialCollection();
+            TextureGrid.DataContext = GetTextureCollection();
+        }
+
+        private ObservableCollection<MeshObject> GetMeshCollection()
+        {
+            var meshes = new ObservableCollection<MeshObject>();
+            meshes.Add(new MeshObject { Name = "Orlando", MeshName = "Gee", VertCnt = 100, MatCnt = 2, Size = 3.25f });
+            meshes.Add(new MeshObject { Name = "Orlando", MeshName = "Gee", VertCnt = 50, MatCnt = 2, Size = 3.25f });
+            meshes.Add(new MeshObject { Name = "Orlando", MeshName = "Gee", VertCnt = 20, MatCnt = 2, Size = 3.25f });
+            return meshes;
+        }
+
+        private ObservableCollection<MaterialObject> GetMaterialCollection()
+        {
+            var materials = new ObservableCollection<MaterialObject>();
+            materials.Add(new MaterialObject { Name = "Orlando", ShaderName = "Gee", RefCnt = 2 });
+            return materials;
+        }
+
+        private ObservableCollection<TextureObject> GetTextureCollection()
+        {
+            var textures = new ObservableCollection<TextureObject>();
+            textures.Add(new TextureObject { Name = "Orlando", PixelSize = "512x512", RefCnt = 2, MemSize = "356KB" });
+            return textures;
         }
     }
 }
