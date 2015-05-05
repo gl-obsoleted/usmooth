f075ab2539ebd0cfe6e5daef690c5559d5b8dd5c
gulu@wfc <gemfeeling@hotmail.com>
2015-05-05 11:30:34 +0800

    import 方式改进，这样任意目录都可以运行这个 py；添加对文件编码的判断(支持 utf-8 with BOM)

--- update_license.py
+++ update_license.py
@@ -11,18 +11,15 @@ Options:
 import os
 import sys
 import string 
+import codecs
 
-sys.path.append('./external/docopt')
+sys.path.append(os.path.join(os.path.dirname(__file__), 'external', 'docopt'))
 from docopt import docopt
 
 LICENSE_FILE = "LICENSE"
 LICENSE_PREFIX = "!lic_info"
 
-header_tmpl = string.Template( \
-"""/*$license_prefix\n
-$license_content
-*/\n
-""")
+header_tmpl = string.Template("""/*$license_prefix\n$license_content\n*/\n""")
 
 def iterate_source_files(directory, extension):
     for root, dirs, files in os.walk(directory):
@@ -30,21 +27,42 @@ def iterate_source_files(directory, extension):
             if name.lower().endswith(extension):
                 yield os.path.join(root, name)
 
+# http://stackoverflow.com/questions/13590749/reading-unicode-file-data-with-bom-chars-in-python
+def detect_by_bom(path,default=None):
+    with open(path, 'rb') as f:
+        raw = f.read(4)    #will read less if the file is smaller
+    for enc,boms in \
+            ('utf-8-sig',(codecs.BOM_UTF8,)),\
+            ('utf-16',(codecs.BOM_UTF16_LE,codecs.BOM_UTF16_BE)),\
+            ('utf-32',(codecs.BOM_UTF32_LE,codecs.BOM_UTF32_BE)):
+        if any(raw.startswith(bom) for bom in boms): return enc
+    return default
+
 def main(args):
     head = ""
     with open(LICENSE_FILE) as f:
         head = header_tmpl.substitute(license_prefix = LICENSE_PREFIX, license_content = f.read())
 
     for src in iterate_source_files(args['--src_dir'], args['--src_ext']):
+    	src_encoding = detect_by_bom(src)
+
         body = ""
         with open(src) as f:
             body = f.read()
+            if src_encoding:
+            	body = body.decode(src_encoding)
 
         if body.startswith('/*'+LICENSE_PREFIX):
             continue
 
+        # print(src)        # for debugging
+        # print(body[:20])
+
         with open(src, "w") as f:
-            f.write(head + body)
+            if src_encoding:
+            	f.write((head + body).encode(src_encoding))
+            else:
+            	f.write(head + body)
 
         print("processed {}".format(src))
 
